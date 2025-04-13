133f0b9e5d5e188db59790c0e2dca3d840a112b1
Aloamoh Alemanji <aloamoh@icloud.com>
2025-04-13 16:34:25 -0400

    my first git hub code

---
+++ bin/electron.dart
@@ -0,0 +1,10 @@
+ enum Months {january, feb, march, april, may, june, july}
+
+ enum Spin {up, down}
+
+ class Electron {
+  int id;
+  static const num = 9.109e-28;
+ }
+ Spin?_spin;
+Electron? _entangtedEtectron;
